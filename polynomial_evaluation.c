0bcca70c6382c5863bbd7089720cd1910f0974c7
yuriQAQ <90495109+yuriQAQ@users.noreply.github.com>
2021-12-06 13:43:39 +0800

    Create polynomial_evaluation.c

---
+++ polynomial_evaluation.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+
+//return x^q
+double power(double x, int q) {
+
+    if (q == 0) {
+        return 1;
+    }
+
+    double m = x;
+    for (int i = 1; i < q; i++) {
+        m *= x;
+    }
+    return m;
+}
+
+int main() {
+    //prompt user to type in x and y
+    printf("請輸入浮點數x的值以及多項式含有的項數正整數y︰");
+
+    //variable declaration
+    double x;
+    int y;
+
+    //get from standard input
+    scanf("%lf%d", &x, &y);
+
+    //initial result
+    double result = 0.0;
+
+    //array declaration, to keep the polynomial with size of 6
+    double p[6];
+    int q[6];
+
+    //iterate each line
+    for (int i = 0; i < y; i++) {
+        //fetch input
+        scanf("%lf%d", &p[i], &q[i]);
+        //value cumulation
+        result += p[i] * power(x, q[i]);
+    }
+
+    printf("%lf\n", result);
+}
