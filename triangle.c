415203be8e7afe5d9bec9c92e3b3656f739ff479
yuriQAQ <90495109+yuriQAQ@users.noreply.github.com>
2022-04-15 15:20:35 +0800

    Update triangle.c

--- triangle.c
+++ triangle.c
@@ -5,9 +5,9 @@ main () {
     for (int i = 0; i < n; i++) {
         for (int j = 0; j < n-i; j++) {
             if (i % 2 ==0) {
-                printf("+");
-            } else {
                 printf("*");
+            } else {
+                printf("+");
             }
         }
         printf("\n");
